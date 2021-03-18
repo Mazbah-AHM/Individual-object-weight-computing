8afa0bed7d10c6b7a5e2c0dfbdc4c391c10c10ff
Mazbah-AHM <ahmazbah6@gmail.com>
2021-03-18 17:25:50 +0800

    second commit

--- Lab1/src/weight2/StreamWeight.java
+++ Lab1/src/weight2/StreamWeight.java
@@ -4,40 +4,43 @@ import java.util.Scanner;
 public class StreamWeight{
 		
 		public static void main(String[] args) {    
-		int n;  
-		Scanner sc=new Scanner(System.in);  
-		System.out.print("Enter the number of elements you want to store: ");  
-		//reading the number of elements from the that we want to enter  
-		n=sc.nextInt();  
-		//creates an array in the memory of length 10  
-		double[] arr1 = new double[n];  
-		System.out.println("Enter the elements of the array: ");  
-		for(int i=0; i<n; i++)  
-		{  
-		//reading array elements from the user   
-		arr1[i]=sc.nextInt();  
-		}     
+			
+			int x;
+		
+			Scanner y = new Scanner(System.in);
+		
+			System.out.print("Enter the number of weights: ");  
+			//getting number of element from user
+			x = y.nextInt();  
+			//initialing an array 
+			double[] array1 = new double[x];  
+			System.out.println("Enter the weights: ");  
+				for(int i=0; i<x; i++)  
+					{  
+						//reading elements of the array. User have to enter the elements.  
+						array1[i]=y.nextInt();  
+					}     
 	      
-	    //Create another array arr2 with size of arr1  
-	    double arr2[] = new double[arr1.length];  
+				//Creating another array which is same size of previous one. 
+				double array2[] = new double[array1.length];  
 	      
-	    //Copying all elements of one array into another  
-	    for (int i = 0; i < arr1.length; i++) {   
-	        arr2[i] = arr1[i]* 0.39;   
-	    }    
+					//storing values of another 
+					for (int i = 0; i < array1.length; i++) {   
+						array2[i] = array1[i]* 0.39;   
+					}    
 	      
-	    //Displaying elements of array arr1   
-	    System.out.println("ON Earth: ");  
-	    for (int i = 0; i < arr1.length; i++) {   
-	       System.out.print(arr1[i] + " ");  
-	    }   
+					//Displaying elements first array   
+					System.out.println("Weight on Earth: ");  
+						for (int i = 0; i < array1.length; i++) {   
+							System.out.print(array1[i] + " ");  
+						}   
 	      
-	    System.out.println();  
+						System.out.println();  
 	      
-	    //Displaying elements of array arr2   
-	    System.out.println("On Mars: ");  
-	    for (int i = 0; i < arr2.length; i++) {   
-	       System.out.print(arr2[i] + " ");  
-	    }   
-	}  
+						//Displaying elements of second array 
+						System.out.println("Weight on Mars: ");  
+							for (int i = 0; i < array2.length; i++) {   
+								System.out.print(array2[i] + " ");  
+							}   
+		}  
 	}
