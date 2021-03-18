0a7d1fc7d2f3fb51c54bf859bf67242119e9e7f5
Mazbah-AHM <ahmazbah6@gmail.com>
2021-03-18 14:06:15 +0800

    Third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class Location {
+	
+	private String nice;
+	
+	public String getNice() {
+		return nice;
+	}
+	public void setNice(String newNice) {
+		this.nice = newNice;
+	}
+	
+}
